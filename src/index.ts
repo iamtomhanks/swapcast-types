cf2281b64e445c932a3028239ceb0d612f11daf7
brec <lebrecdesign@gmail.com>
2020-04-08 12:23:28 -0400

    export all

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import * as Requests from './Interfaces/Requests';
 import * as ServerRoutes from './Interfaces/Server/routes';
 import * as DBTables from './Interfaces/Server/tables';
+import * as Form from './Interfaces/Form';
+import * as Participant from './Interfaces/Participant';
+import * as Redux from './Interfaces/redux';
 
 export {
   Requests,
   ServerRoutes,
-  DBTables
+  DBTables,
+  Form,
+  Participant,
+  Redux
 };
