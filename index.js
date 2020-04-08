a6457fcb32c08525e0e652d086e28cf24ff9636b
brec <lebrecdesign@gmail.com>
2020-04-08 11:33:22 -0400

    use babel

--- index.js
+++
@@ -1,5 +0,0 @@
-const requests = require('./src/Interfaces/Requests');
-
-module.exports = {
-  requests,
-};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as requests from './src/Interfaces/Requests';
+
+module.exports = {
+  requests,
+};
